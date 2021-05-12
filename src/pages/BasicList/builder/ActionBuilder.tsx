fbc80dfaa48c2cb1fd8bd9bf2f9a9859ac97b95e
小喵喵 <2449968976@qq.com>
2021-05-12 17:13:30 +0800

    record

--- src/pages/BasicList/builder/ActionBuilder.tsx
+++ src/pages/BasicList/builder/ActionBuilder.tsx
@@ -2,28 +2,29 @@ import { Button } from 'antd';
 import type { ButtonType } from 'antd/lib/button';
 
 const ActionBuilder = (
-  actions: BasicListApi.Action[] | undefined,
-  actionHandler: BasicListApi.ActionHandler,
-  loading: boolean,         //判断是否已经穿入完成，所以是boolean类型
-  record: any,
+    actions: BasicListApi.Action[] | undefined,
+    actionHandler: BasicListApi.ActionHandler,
+    //   loading: boolean,         //判断是否已经穿入完成，所以是boolean类型
+    loading: false,    //直接给默认值，一避免无值报错，二，可以不用在传false这个值
+    record: {},      //空对象
 ) => {
-  return (actions || []).map((action) => {
-    if (action.component === 'button') {
-      return (
-        <Button
-          key={action.text}
-          type={action.type as ButtonType}
-          onClick={() => {
-            actionHandler(action, record);
-          }}
-          loading={loading}               //点击一次按钮之后进入loading状态，避免用户多次提交    loading={loading}  
-        >
-          {action.text}
-        </Button>
-      );
-    }
-    return null;
-  });
+    return (actions || []).map((action) => {
+        if (action.component === 'button') {
+            return (
+                <Button
+                    key={action.text}
+                    type={action.type as ButtonType}
+                    onClick={() => {
+                        actionHandler(action, record);
+                    }}
+                    loading={loading}               //点击一次按钮之后进入loading状态，避免用户多次提交    loading={loading}  
+                >
+                    {action.text}
+                </Button>
+            );
+        }
+        return null;
+    });
 };
 export default ActionBuilder;
 
